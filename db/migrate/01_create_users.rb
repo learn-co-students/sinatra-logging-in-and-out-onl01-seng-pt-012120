59d559b92e0e2f6f58b41b0d7b6f1d451baf0a94
github username <github email address>
2020-04-29 14:16:51 -0700

    Done.

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -1,9 +1,9 @@
-class CreateUsers < ActiveRecord::Migration[5.2]
+class CreateUsers < ActiveRecord::Migration[5.1]
     def change
       create_table :users do |t|
         t.string :username 
         t.string :password
-        t.float  :balance
+        t.decimal :balance
    
       end
     end
